d4336e5458b30b990b4c12f72f3b7a1c87924510
Tim Padjen <tpadjen@gmail.com>
2016-08-30 11:11:03 -0600

    Add instructions and improve formatting

--- methods.rb
+++ methods.rb
@@ -1,41 +1,49 @@
-module Methods
-	
-	
-	# sleep_in?
-	#
-	# Students sleep in if it is not a weekday or we're on vacation. 
-	#
-	# ==== Attributes 
-	# 
-	# * weekday  - true only if it is a weekday
-	# * vacation - true only if the student is on vacation
-	#
-	# TODO - write sleep_in?
+# *****************************************************
+#
+# 	Methods
+#
+# 		Write the three methods below. Use the tests in
+# 		`methods_test.rb` to verify that they work
+# 		correctly. DO NOT print any info to the console.
+#
+# *****************************************************
 
-	# monkey_trouble?
-	#
-	# We have two monkeys, a and b, and the parameters a_smile and b_smile 
-	# indicate if each is smiling. 
-	# We are in trouble if they are both smiling or if neither of them is smiling.
-	# Return true if we are in trouble.
-	#
-	# ==== Attributes 
-	# 
-	# * a_smile   - true only if monkey a is smiling
-	# * b_smile   - true only if monkey b is smiling
-	#
-	# TODO - write monkey_trouble?
 
-	# sum_double
-	#
-	# Given two int values, return their sum. Unless the two values are the same,
-	# then return double their sum. 
-	#
-	# ==== Attributes 
-	# 
-	# * a - an integer
-	# * b - an integer
-	#
-	# TODO - write sum_double
+# sleep_in?
+#
+# Students sleep in if it is not a weekday or we're on vacation. 
+#
+# > Parameters
+# ------------ 
+# 	* weekday  - true only if it is a weekday
+# 	* vacation - true only if the student is on vacation
+#
+# TODO - write sleep_in?
 
-end
+
+# monkey_trouble?
+#
+# We have two monkeys, a and b, and the parameters a_smile and b_smile 
+# indicate if each is smiling. 
+# We are in trouble if they are both smiling or if neither of them is smiling.
+# Return true if we are in trouble.
+#
+# > Parameters 
+# ------------
+# 	* a_smiling   - true only if monkey a is smiling
+# 	* b_smiling   - true only if monkey b is smiling
+#
+# TODO - write monkey_trouble?
+
+
+# sum_double
+#
+# Given two int values, return their sum. Unless the two values are the same,
+# then return double their sum. 
+#
+# > Parameters 
+# ------------
+# 	* a - an integer
+# 	* b - an integer
+#
+# TODO - write sum_double
